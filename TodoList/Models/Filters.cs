8b5e80345194660ba8a7b8b94de53d9cbd98b192
ADMIN <hoangthuyktv123@gmail.com>
2024-05-31 01:27:41 +0700

    update

---
+++ TodoList/Models/Filters.cs
@@ -0,0 +1,36 @@
+﻿namespace TodoList.Models
+{
+    public class Filters
+    {
+        public Filters(string filterstring)
+        {
+            Filterstring = filterstring ?? "all-all-all";
+            string[] filters = Filterstring.Split('-');
+            CategoryId = filters[0];
+            Due = filters[1];
+            StatusId = filters[2];
+        }
+        public string Filterstring { get; set; }
+        public string CategoryId { get; set; }
+
+        public string Due { get; set; }
+        public string StatusId { get; }
+        public bool HasCategory => CategoryId.ToLower() != "all";
+
+        public bool HasDue => Due.ToLower() != "all";
+
+        public bool HasStatus => StatusId.ToLower() != "all";
+
+        public static Dictionary<string, string> DueFilterValues =>
+            new Dictionary<string, string> {
+                    { "future", "Future " },
+                    {"past", "Past" },
+                    {"today", "Today" }
+
+            };
+        public bool IsPart => Due.ToLower() == "past";
+        public bool IsFuture => Due.ToLower() == "future";
+        public bool IsToday => Due.ToLower() == "today";
+    }
+
+}
---
+++ TodoList/Models/TodoContext.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+namespace TodoList.Models
+{
+    public class TodoContext : DbContext
+    {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            //seed data
+            modelBuilder.Entity<Category>().HasData(
+                new Category { CategoryId = "work", Name = "Work " },
+                new Category { CategoryId = "home", Name = "home " },
+                new Category { CategoryId = "ex", Name = "Exercise " },
+                new Category { CategoryId = "shop", Name = "shopping " },
+                new Category { CategoryId = "call", Name = "contact " }
+            );
+
+            modelBuilder.Entity<Status>().HasData(
+                new Status { StatusId = "open", StatusName = "open" },
+                new Status { StatusId = "closed", StatusName = "Complete" }
+                );
+        }
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+        public DbSet<Todo> Todos { get; set; } = null!;
+        public DbSet<Category> Categories { get; set; } = null!;
+        public DbSet<Status> Statuses { get; set; } = null!;
+        
+        
+    }
+}
